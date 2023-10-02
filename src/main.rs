93c8dbe172a6dc3a9f3d48c61b6cec688b1a3b19
kevin-koshy <kevin_koshy@hotmail.com>
2023-10-01 21:44:19 -0400

    Adding Tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{env,process};
-use std::fs;
-use std::error::Error;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String>= env::args().collect();
@@ -11,34 +10,13 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config){
+    if let Err(e) = minigrep::run(config){
         println!("Application error: {}", e);
         process::exit(10);
     }
 
-
-    fn run(config: Config)  -> Result<(), Box<dyn Error>> {
-        let contents = fs::read_to_string(config.file_path)?;
-        println!("With text:\n{}", contents);
-        Ok(())
-    }
-
-    let file_path = "/Users/kevinkoshy/Documents/Development/Rust/RustBook/minigrep/poem.txt";
-    println!("In file {}", file_path);
+    // let file_path = "/Users/kevinkoshy/Documents/Development/Rust/RustBook/minigrep/poem.txt";
+    // println!("In file {}", file_path);
 
 
-}
-struct Config {
-    query: String,
-    file_path: String,
-}
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
 }
